d5608d40e17ecb73dce576f681fd91a9ca00b251
Hawraa <hawraalasad@yahoo.com>
2024-09-18 13:41:03 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <table className="">
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
